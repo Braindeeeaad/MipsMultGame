3236f4353a7e4450b7f28632d536f72ad79520c6
Braindeeeaad <Indrajith2048@gmail.com>
2025-04-29 05:05:22 -0500

    It Fucking Works

--- Pointer.asm
+++ Pointer.asm
@@ -5,10 +5,6 @@
 .data
                 .globl      left_border
                 .globl      right_border
-array:          .space      36      # 9 words (aligned)
-firstpointer:   .word      0        # 4 bytes (aligned)
-secpointer:     .word      0        # 4 bytes (aligned)
-matrixpointer:  .space      144     # 36 words (aligned)
 left_border:    .asciiz     "| "
 separator:      .asciiz     " | "
 right_border:   .asciiz     " |"
@@ -19,38 +15,6 @@ space:          .asciiz     " "
     #
     #Need to do move validation(linear-search->returns(-1, then invalid move)
     #Need an array from [1,...,9]
-                .globl      main
-main:
-    la      $a0,                            array
-    jal     init_array
-    #jal print_array
-    la      $a0,                            firstpointer
-    la      $a1,                            secpointer
-    jal     init_pointers
-
-
-    la      $a0,                            matrixpointer                                                           # Load matrix pointer address
-    jal     init_matrix                                                                                             # Initialize with our data
-
-    la      $a0,                            firstpointer
-    la      $a1,                            secpointer
-    la      $a2,                            3
-    la      $a3,                            4
-    li      $t0,                            -2
-    la      $t1,                            matrixpointer
-    jal     move_pointers
-
-    la      $a0,                            matrixpointer
-    jal     print_matrix
-
-
-
-    la      $a0,                            firstpointer
-    la      $a1,                            secpointer
-    la      $a2,                            array
-    jal     print_pointers
-    li      $v0,                            SysExit
-    syscall
 
 
 
